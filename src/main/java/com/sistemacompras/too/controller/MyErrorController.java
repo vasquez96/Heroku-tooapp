5898e1ca299ac6588d7339bfbb87d4052f6f1186
GerardoPeralta21 <47636271+GerardoPeralta21@users.noreply.github.com>
2020-12-07 23:08:11 -0600

    Se controla error 403

--- src/main/java/com/sistemacompras/too/controller/MyErrorController.java
+++ src/main/java/com/sistemacompras/too/controller/MyErrorController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class MyErrorController implements ErrorController {
 
-
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
